fc55c2655496442a08df23226943bda78e1f6fea
Joshrobertson023 <therealjoshrobertson@gmail.com>
2023-10-28 20:18:42 -0500

    Basic operations working. Still need to add a couple things.
    Still need to add the negate button and decimal button.

--- script.js
+++ script.js
@@ -15,17 +15,20 @@
 const btnEquals = document.getElementById('='),
       display = document.getElementById('display'),
       btnOperators = Array.from(document.querySelectorAll('.operator')),
-      btnNumbers = Array.from(document.querySelectorAll('.num'));
+      btnNumbers = Array.from(document.querySelectorAll('.num')),
+      btnAC = document.getElementById('ac'),
+      btnC = document.getElementById('c'),
+      btnNegate = document.getElementById('n');
 
 /**********************************************************************/
 /*                          Global variables                          */
 /**********************************************************************/
 let firstNumber = 0,
     secondNumber = 0,
-    operator = '',
     result = 0,
-    array = '',
-    currentValue = 0;
+    memory = '',
+    currentValue = 0,
+    operator = '';
 
 /**********************************************************************/
 /*                            Main progrma                            */
@@ -34,69 +37,57 @@ let firstNumber = 0,
 btnNumbers.forEach(function(num) {
    num.addEventListener('click', function() {
       display.textContent += num.textContent;
-   })
-})
+      memory += num.textContent;
+   });
+});
 // When the operator is pressed, add to display and array
+btnOperators.forEach(function(op) {
+   op.addEventListener('click', function() {
+      display.textContent += op.textContent;
+      memory += '~' + op.textContent + '~';
+   });
+});
+
+btnEquals.addEventListener('click', function() {
+   let fields = memory.split('~');
+   firstNumber = fields[0];
+   operator = fields[1];
+   secondNumber = fields[2];
+
+   let result = operate(firstNumber, operator, secondNumber);
+   display.textContent = result;
+});
+
+btnAC.addEventListener('click', function() {
+   display.textContent = '0';
+   memory = '';
+});
+
+btnC.addEventListener('click', function() {
+   display.textContent = '0';
+});
+
+btnNegate.addEventListener('click', function() {
+   // Add code to negate
+});
 // When the next number button is pressed, add to display and array
 // If array gets another operator, operate on last expression
 // If equals is pressed, operate on last expression
 
 /**********************************************************************/
-/*                      Basic operation functions                     */
-/**********************************************************************/
-function add(a, b) {
-   return a + b;
-}
-
-function subtract(a, b) {
-   return a - b;
-}
-
-function multiply(a, b) {
-   return a * b;
-}
-
-function divide(a, b) {
-   return a / b;
-}
-
-function modulus(a, b) {
-   return a % b;
-}
-
-function negate(num) { // Need to place this function somewhere still
-   return num * -1;
-}
-
-/**********************************************************************/
 /*                           Operate function                         */
 /**********************************************************************/
 function operate(firstNumber, operator, secondNumber) {
-   let result = 0;
    switch(operator) {
       case '+':
-         result = add(firstNumber, secondNumber);
-         break;
+         return parseInt(firstNumber) + parseInt(secondNumber);
       case '-':
-         result = subtract(firstNumber, secondNumber);
-         break;
+         return parseInt(firstNumber) - parseInt(secondNumber);
       case '*':
-         result = multiply(firstNumber, secondNumber);
-         break;
+         return parseInt(firstNumber) * parseInt(secondNumber);
       case '/':
-         result = divide(firstNumber, secondNumber);
-         break;
+         return parseInt(firstNumber) / parseInt(secondNumber);
       case '%':
-         result = modulus(firstNumber, secondNumber);
-         break;
+         return parseInt(firstNumber) % parseInt(secondNumber);
    }
-   return result;
-}
-
-function populateDisplay(value) {
-   display.textContent = value;
-}
-
-function addToArray(value) {
-   array += value;
 }
